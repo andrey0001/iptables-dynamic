0e0e9645194e6fa3160aaa329fcfc0a45906691d
andrey0001 <sa@sorokin.org>
2022-05-08 00:35:17 +0300

    first commit

---
+++ trustlist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Set path for ipset and ipsetd
+IPSET="/usr/sbin/ipset"
+IPSETD="/root/bin/ipsetd"
+
+# Add domain to array
+DomainsArray=("dns.google.com" "cnn.com")
+
+# Use command-line argument to provide ipset list name (Default: trust-ip)
+REALLISTNAME=$1
+[ -z "$1" ] && REALLISTNAME="trust-ip"
+
+# Creation of temporary ipset list name
+LISTNAME="list-"`date +%s`
+
+$IPSET create $LISTNAME hash:ip
+
+#Add domains to ipset list
+for str in ${DomainsArray[@]}; do
+  $IPSETD $LISTNAME $str
+done
+
+$IPSET swap $LISTNAME $REALLISTNAME
+$IPSET destroy $LISTNAME
+
